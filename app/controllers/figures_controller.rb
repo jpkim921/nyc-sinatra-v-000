3f87c0302a6895a28371cf725204ed55355316ac
Jayon Kim <jpkim921@gmail.com>
2018-06-09 20:25:58 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,5 +1,6 @@
 class FiguresController < ApplicationController
 
+<<<<<<< HEAD
   get '/figures/new' do
     erb :'/figures/new'
   end
@@ -53,4 +54,6 @@ class FiguresController < ApplicationController
     redirect "/figures/#{@figure.id}"
   end
 
+=======
+>>>>>>> 32642b0f48d0690af3a71081cb99aa7b7fcd0ae9
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,6 +1,34 @@
 require 'pry'
 
 class LandmarksController < ApplicationController
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    # binding.pry
+    redirect '/landmarks/index'
+  end
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+
+  end
 
   get '/landmarks/new' do
     erb :'landmarks/new'
